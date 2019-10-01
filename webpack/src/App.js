1f27aff95bad52834a0050595c6fa517e61a0eb8
Sergei <sepolish@student.jyu.fi>
2019-10-01 04:52:26 +0300

    harjoittelen webpackia

---
+++ webpack/src/App.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react'
+// import axios from 'axios'
+
+const App = () => {
+  const [counter, setCounter] = useState(0)
+  const [values, setValues] = useState([])
+
+  const handleClick = () => {
+    setCounter(counter + 1)
+    setValues(values.concat(counter))
+  }
+
+  return (
+    <div className="container">
+      hello webpack {counter} clicks
+      <button onClick={handleClick} >press</button>
+    </div>
+  )
+}
+
+export default App
